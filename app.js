bab211dad3c6e8255a4ff11a91c85c216c9a28b4
Azzie Fuzzie <azziefuzzie01@gmail.com>
2023-01-20 01:00:41 +0200

    app.js
    Prismic

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import * as prismicH from '@prismicio/helpers';
 import { client } from './config/prismicConfig.js';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(express.static('public'));
 app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.locals.ctx = {
@@ -19,10 +22,31 @@ app.use((req, res, next) => {
   next();
 });
 
+const handleRequest = async () => {
+  const home = await client.getSingle('home');
+
+  const assets = [];
+
+  home.data.body.forEach((section) => {
+    if (section.slice_type === 'barbers') {
+      console.log(section.items[0]);
+      assets.push(section.items[0]);
+      assets.push(section.items[1]);
+      assets.push(section.items[2]);
+      assets.push(section.items[3]);
+      console.log(assets);
+    }
+  });
+
+  return {
+    home,
+    assets,
+  };
+};
+
 app.get('/', async (req, res) => {
-  const home = await client.getFirst();
-  console.log(home.data.body);
-  res.render('pages/home', { home });
+  const defaults = await handleRequest();
+  res.render('pages/home', { ...defaults });
 });
 
 app.get('/about', (req, res) => {
--- webpack.config.build.cjs
+++ webpack.config.build.cjs
@@ -1,6 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const config = require('./webpack.config.js');
+const config = require('./webpack.config.cjs');
 
 module.exports = merge(config, {
   mode: 'production',
